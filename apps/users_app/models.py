823afde1259361603eb51c4103a11862427cb767
Felipe <felipeo510.fs@gmail.com>
2017-09-19 21:26:07 -0700

    first orm assignment

---
+++ apps/users_app/models.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Users(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    age = models.PositiveIntegerField()
+    crated_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
